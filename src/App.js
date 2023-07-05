1711c7202402d10fcc791dc67cc49ae79420b678
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-07-05 13:52:12 +0330

    modified:   src/App.js 	modified:   src/components/Navbar.jsx 	modified:   src/components/Sidebar.jsx 	new file:   src/contexts/ContextProvider.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
 import {
   ECommerce,
@@ -23,9 +22,10 @@ import {
   Editor,
 } from "./pages";
 import "./App.css";
-
+import { useStateContext } from "./contexts/ContextProvider";
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
+
   return (
     <div>
       <BrowserRouter>
